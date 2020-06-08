cf3e77677cb5090acdf79e23483b124ce0669b4a
Dan <danharasymiw@gmail.com>
2020-06-08 00:01:09 -0400

    create client and server

---
+++ client.py
@@ -0,0 +1,15 @@
+import random, requests, time
+
+url = 'http://localhost:7777/number'
+
+def make_request():
+    number = random.randint(1, 101)
+    try:
+        requests.post(url, data={'number': number})
+    except:
+        print('request failed')
+    print('request sent')
+
+while True:
+    make_request()
+    time.sleep(5)
